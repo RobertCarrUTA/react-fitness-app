9a88248bea6a67384195ff9a30f7cc7301d080aa
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-06 00:21:25 -0500

    Added some stuff to get the fetch data working
    I still need to add more but I think I will take a break for now.

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Exercises from "./Exercises";
+
 const SearchExercises = () => {
   const [search, setSearch] = useState("");
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      // const bodyPartsData = await fetchData('')
+    };
+  }, []);
 
   const handleSearch = async () => {
     if (search) {
-      // const exercisesData = await fetchData();
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+
+      const searchedExercises = exerciseData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+      );
+
+      setSearch("");
+      setExercises(searchedExercises);
     }
   };
 
