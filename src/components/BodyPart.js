bd9d90bf4286d6b2616bd42a95ac3b87993a778f
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-06 17:35:50 -0500

    Set up the basis for the scrollbar, now to populate it
    The basis for the scrollbar has been added, but now it just needs to be populated.

---
+++ src/components/BodyPart.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+
+import Icon from "../assets/icons/gym.png";
+
+const BodyPart = () => {
+  return <div>BodyPart</div>;
+};
+
+export default BodyPart;
