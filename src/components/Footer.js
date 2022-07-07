df9739ece10d935f878e70c014d9487a963d4e84
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-07 06:00:37 -0500

    The footer has been added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="20px">
+          Made by Robert as part of a learning project
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
