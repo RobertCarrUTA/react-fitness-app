8fdc986c2f321eb54558d74bae03b9ea1ff858e8
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-07 00:57:32 -0500

    Related YouTube videos are now shown
    Related YouTube videos are now shown to the user when they click to see more details about an exercise.

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const ExerciseVideos = () => {
-  return <div>ExerciseVideos</div>;
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return "Loading...";
+
+  return (
+    <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
+      <Typography variant="h3" mb="33px">
+        Watch{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          {name}
+        </span>{" "}
+        exercise videos
+      </Typography>
+      <Stack
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+        sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0" } }}
+      >
+        {exerciseVideos?.slice(0, 3).map((item, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+          </a>
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default ExerciseVideos;
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import Detail from "../components/Detail";
 import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -24,6 +26,12 @@ const ExerciseDetail = () => {
         exerciseOptions
       );
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youTubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData.contents);
     };
     fetchExercisesData();
   }, [id]);
@@ -31,7 +39,10 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
       <SimilarExercises />
     </Box>
   );
